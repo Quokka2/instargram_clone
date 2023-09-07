66704ff6b6cca43b191e767f764daa7da3d5aa7b
home <gkadudgnl@gmail.com>
2023-09-07 16:13:30 +0900

    css,img,js 로그인 상관없이 승인설정

--- src/main/java/com/example/instargram/config/SecurityConfig.java
+++ src/main/java/com/example/instargram/config/SecurityConfig.java
@@ -1,33 +1,38 @@
 package com.example.instargram.config;
 
 import com.example.instargram.domain.UserRole;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/security-login/info").authenticated()
-                .antMatchers("/security-login/admin/**").hasAuthority(UserRole.ADMIN.name())
-                .anyRequest().permitAll()
-                .and()
-                .formLogin()
-                .usernameParameter("loginId")
-                .passwordParameter("password")
-                .loginPage("/security-login/login")
-                .defaultSuccessUrl("/security-login")
-                .failureUrl("/security-login/login")
-                .and()
-                .logout()
-                .logoutUrl("/security-login/logout")
-                .invalidateHttpSession(true).deleteCookies("JSESSIONID");
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+				.authorizeRequests()
+				.antMatchers("/css/**").permitAll()
+				.antMatchers("/img/**").permitAll()
+				.antMatchers("/js/**").permitAll()
+				.antMatchers("/security-login/info").authenticated()
+				.antMatchers("/security-login/admin/**").hasAuthority(UserRole.ADMIN.name())
+				.anyRequest().permitAll()
+				.and()
+				.formLogin()
+				.usernameParameter("loginId")
+				.passwordParameter("password")
+				.loginPage("/security-login/login")
+				.defaultSuccessUrl("/security-login")
+				.failureUrl("/security-login/login")
+				.and()
+				.logout()
+				.logoutUrl("/security-login/logout")
+				.invalidateHttpSession(true).deleteCookies("JSESSIONID");
+		return http.build();
 
-    }
+	}
 }
