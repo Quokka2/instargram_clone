0c25ca6467f84db24d25729ff9678fd4f85383fd
home <gkadudgnl@gmail.com>
2023-09-13 14:46:35 +0900

    게시판 Member 추가

---
+++ src/main/java/com/example/instargram/domain/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.example.instargram.domain;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<User, Long> {
+    boolean existsByLoginId(String loginId);
+    boolean existsByNickname(String nickname);
+    Optional<User> findByLoginId(String loginId);
+}
