5fe7977afac4a95bef4731b101aee58d54ff9f00
home <gkadudgnl@gmail.com>
2023-10-12 13:16:29 +0900

    컨트롤러 추가

---
+++ src/main/java/com/example/instargram/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.example.instargram.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class AuthController {
+//	private final AuthService authService;
+
+	@GetMapping({"/login" , "/"})
+	public String LoginForm(){
+		return "login";
+	}
+
+	@GetMapping("/signup")
+	public String SignupForm(){
+		return "signup";
+	}
+
+//	@PostMapping("/signup")
+//	public String Signup(UserLoginDto userLoginDto){
+//		User user = userLoginDto.toEntity();
+//
+//		User userEntity = authService.signup(userLoginDto);
+//		if(userEntity == null){
+//			return "redirect:/signup?error";
+//		}
+//		return "redirect:/login";
+//	}
+}
